f301dca5576964d22aa14e5ebba3e07d32fec5b9
Dawid Mucha <dawidmucha98@gmail.com>
2023-07-27 20:58:50 +0200

    enviromental variables finally work

--- src/components/TimeTable.vue
+++ src/components/TimeTable.vue
@@ -2,7 +2,7 @@
 /* eslint-disable */
 	import { useDateFormat, useNow } from '@vueuse/core'
 	import { collection, getDocs } from 'firebase/firestore'
-	import db from '../main'
+
 
 	const formattedDate = useDateFormat(useNow(), 'dddd, DD MMMM, YYYY')
 </script>
@@ -17,11 +17,6 @@
 </template>
 
 <script>
-	const querySnapshot = await getDocs(collection(db, 'testing'))
-	// querySnapshot.forEach((doc) => {
-	// 	console.log(`${doc.id} => doc.data()`)
-	// })
-
 	export default {
 		name: 'TimeTable',
 		data() {
--- src/main.js
+++ src/main.js
@@ -3,21 +3,27 @@ import App from './App.vue'
 
 import { initializeApp } from "firebase/app"
 import { getFirestore, collection } from 'firebase/firestore'
-// import { getAnalytics } from "firebase/analytics"
+import { VueFire, VueFireAuth } from 'vuefire'
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+export const firebaseApp = initializeApp({
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  // apiKey: "AIzaSyAkwYPvUa26zAJEgbHTzHyRIRO9Vwrqvog",
   authDomain: "time-blocks-5813f.firebaseapp.com",
   projectId: "time-blocks-5813f",
   storageBucket: "time-blocks-5813f.appspot.com",
   messagingSenderId: "64523075542",
   appId: "1:64523075542:web:d1a5f4ddab37f1608fee77",
   measurementId: "G-31Q1PD6DPJ"
-}
-const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
-// const analytics = getAnalytics(app)
+})
 
-createApp(App).mount('#app')
-
+const db = getFirestore(firebaseApp)
 export const testingRef = collection(db, 'testing')
+
+const app = createApp(App)
+app.use(VueFire, {
+  firebaseApp,
+  modules: [
+    VueFireAuth(),
+  ],
+})
+app.mount('#app')
